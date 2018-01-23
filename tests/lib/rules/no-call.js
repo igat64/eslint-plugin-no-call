c37cf734a5a7e1429a8e0b2f934acc845a6bb3de
Igor Atroshkin <igor.atroshking@gmail.com>
2018-01-23 19:18:35 +0300

    implemented for simple fn call and method call

---
+++ tests/lib/rules/no-call.js
@@ -0,0 +1,104 @@
+/**
+ * @fileoverview TODO: add leter
+ * @author igor
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/no-call");
+var RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Helpers
+//------------------------------------------------------------------------------
+
+function funcError(functionName) {
+  return {
+    ruleId: "no-call",
+    message: "The call of function `" + functionName + "` is restricted by configuration."
+  };
+}
+function methodError(methodNameWithCalleObj) {
+  return {
+    ruleId: "no-call",
+    message: "The call of method `" + methodNameWithCalleObj + "` is restricted by configuration."
+  };
+}
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run("no-call", rule, {
+  valid: [
+    //----------------------------------------------------------------------
+    // Valid Functions Calls
+    //----------------------------------------------------------------------
+    "call()",
+    {
+      code: "anyFunctionCall()",
+      options: [["obj.anyFunctionCall"]]
+    },
+    {
+      code: "anyFunctionCall()",
+      options: [[]]
+    },
+    {
+      code: "new F()",
+      options: [["F", "new F"]]
+    },
+    {
+      code: "(function fn() {})()",
+      options: [["fn"]]
+    },
+    // "(() => {})()",
+    "(function () {})()",
+    //----------------------------------------------------------------------
+    // Valid Methods Calls
+    //----------------------------------------------------------------------
+    {
+      code: "_.set.call(_)",
+      options: []
+    },
+    {
+      code: "_.set.call(_)",
+      options: [["set.call"]]
+    },
+    {
+      code: "var set = _.set; set();",
+      options: [["_.set"]]
+    }
+  ],
+
+  invalid: [
+    //----------------------------------------------------------------------
+    // Invalid Functions Calls
+    //----------------------------------------------------------------------
+    {
+      code: "setIn()",
+      options: [["setIn", "set"]],
+      errors: [funcError("setIn")]
+    },
+    //----------------------------------------------------------------------
+    // Invalid Methods Calls
+    //----------------------------------------------------------------------
+    {
+      code: "_.set({}, 'k', 'v')",
+      options: [["_.set"]],
+      errors: [methodError("_.set")]
+    },
+    {
+      code: "some.deep.set({}, 'k', 'v')",
+      options: [["some.deep.set"]],
+      errors: [methodError("some.deep.set")]
+    },
+    {
+      code: "_.set.call(_)",
+      options: [["_.set.call"]],
+      errors: [methodError("_.set.call")]
+    }
+  ]
+});
